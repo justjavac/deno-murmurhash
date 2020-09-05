9b6a728a185bdcbfc04e04aeff572686bd5d69f2
justjavac <justjavac@gmail.com>
2020-09-06 00:51:18 +0800

    upgrade std@0.68.0

--- murmurhash_test.ts
+++ murmurhash_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 import Murmurhash3 from "./mod.ts";
 
